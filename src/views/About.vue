95eb1bb9f6fe15702b64b40187b26580a1f425a6
RodgersX <brianmawira2@gmail.com>
2022-01-22 12:39:56 +0300

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi aliquid
+      dolorum labore dolorem, sit, delectus sapiente in rerum, fugit eaque
+      voluptates minima at obcaecati? In illum voluptates, dignissimos quaerat
+      exercitationem voluptate asperiores quam modi est porro et quae ab debitis
+      laboriosam numquam, itaque sit, soluta fugiat minima molestiae aut quod.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi aliquid
+      dolorum labore dolorem, sit, delectus sapiente in rerum, fugit eaque
+      voluptates minima at obcaecati? In illum voluptates, dignissimos quaerat
+      exercitationem voluptate asperiores quam modi est porro et quae ab debitis
+      laboriosam numquam, itaque sit, soluta fugiat minima molestiae aut quod.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi aliquid
+      dolorum labore dolorem, sit, delectus sapiente in rerum, fugit eaque
+      voluptates minima at obcaecati? In illum voluptates, dignissimos quaerat
+      exercitationem voluptate asperiores quam modi est porro et quae ab debitis
+      laboriosam numquam, itaque sit, soluta fugiat minima molestiae aut quod.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi aliquid
+      dolorum labore dolorem, sit, delectus sapiente in rerum, fugit eaque
+      voluptates minima at obcaecati? In illum voluptates, dignissimos quaerat
+      exercitationem voluptate asperiores quam modi est porro et quae ab debitis
+      laboriosam numquam, itaque sit, soluta fugiat minima molestiae aut quod.
+    </p>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+<style scoped></style>
